15d55b58f3b755c1676932de4119f02060f652c8
Robbert Broersma <git@robbertbroersma.nl>
2023-12-14 13:25:24 +0100

    feat: validate each `publiccode.yml` when no input is specified

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,12 @@ set -o pipefail
 
 # comment-on-pr parameter from the action
 if [ "$2" == "true" ]; then
+    echo "Validate one file and comment on PR"
     pcvalidate "$1" | reviewdog -efm="%f:%l:%c: %t%*[^:]: %m" -reporter=github-pr-review -tee
-else
+elif [ ! -z "$1" ]; then
+    echo "Validate one file"
     pcvalidate "$1"
+else
+    echo "Validate one each publiccode.yml in the working directory or any subdirectories"
+    find . -type d \( -name node_modules -or -name dist \) -prune -o \( -name 'publiccode.yaml' -or -name 'publiccode.yml' \) -print0 | xargs -0 pcvalidate $1
 fi
